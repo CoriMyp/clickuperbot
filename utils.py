f9ae6e5aaa457f8262647a898f22143e6e7f5e68
Ser G <s.kam41@yandex.com>
2025-11-06 12:02:46 +0700

    Make `is_bot_mentioned` case insensitive and reformat code in `utils.py`

--- utils.py
+++ utils.py
@@ -5,30 +5,41 @@ import json
 import config
 
 
-def is_bot_mentioned(msg: types.Message):
-	if msg.text.startswith((config.BOT_NAME, config.BOT_USERNAME)):
-		return True
-	return None
+def is_bot_mentioned(msg: types.Message) -> bool:
+    """
+    Checks if the message starts with the bot's name or username (case insensitive).
+    """
+    text = msg.text or ""
+    bot_names = (config.BOT_NAME.lower(), config.BOT_USERNAME.lower())
+    return text.lower().startswith(bot_names)
 
 
-def clean_up_text(text: str):
-	if text.startswith(config.BOT_NAME):
-		text = text.split(config.BOT_NAME, maxsplit=1)[1]
-	else:
-		text = text.split(config.BOT_USERNAME, maxsplit=1)[1]
-	return text.strip()
+def clean_up_text(text: str) -> str:
+    """
+    Cleans up the text of the message by removing the bot's name or username.
+    """
+    if text.startswith(config.BOT_NAME):
+        text = text.split(config.BOT_NAME, maxsplit=1)[1]
+    else:
+        text = text.split(config.BOT_USERNAME, maxsplit=1)[1]
+    return text.strip()
 
 
-def parse_json(text: str):
-	try:
-		return json.loads(text)
-	except json.decoder.JSONDecodeError:
-		raise Exception(f"[JSON] can't convert: {text}")
-	
+def parse_json(text: str) -> dict:
+    """
+    Parses a JSON string and returns a dictionary.
+    """
+    try:
+        return json.loads(text)
+    except json.decoder.JSONDecodeError:
+        raise Exception(f"[JSON] can't convert: {text}")
 
-async def error_msg(bot: Bot, msg: types.Message, text):
-	await msg.react(reaction=[types.ReactionTypeEmoji(emoji="💔")])
 
-	for admin in config.BOT_ADMINS:
-		await bot.send_message(admin, text,
-		parse_mode=ParseMode.MARKDOWN_V2)
+async def error_msg(bot: Bot, msg: types.Message, text: str) -> None:
+    """
+    Sends an error message to the admins.
+    """
+    await msg.react(reaction=[types.ReactionTypeEmoji(emoji="💔")])
+
+    for admin in config.BOT_ADMINS:
+        await bot.send_message(admin, text, parse_mode=ParseMode.MARKDOWN_V2)
